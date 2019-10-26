e83d66dba5b62676ca57698fb2f1fdd0d6c87c9a
notmarkmiranda <notmarkmiranda@gmail.com>
2019-10-26 14:38:26 -0600

    create initial admin and season

---
+++ Poker Rank Swift/Models/Membership.swift
@@ -0,0 +1,15 @@
+//
+//  Membership.swift
+//  Poker Rank Swift
+//
+//  Created by Mark Miranda on 10/16/19.
+//  Copyright © 2019 Mark Miranda. All rights reserved.
+//
+
+import Foundation
+
+struct Membership: Codable {
+    var league_id: String
+    var user_id: String
+    var role: Int
+}
--- Poker Rank Swift/ViewControllers/NewLeagueViewController.swift
+++ Poker Rank Swift/ViewControllers/NewLeagueViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import FirebaseAuth
 import FirebaseFirestore
+import CodableFirebase
 
 protocol NewLeagueViewControllerDelegate: AnyObject {
   func appendNewLeague(_ league: League)
@@ -47,7 +48,6 @@ class NewLeagueViewController: UIViewController {
           if let documentId = ref?.documentID {
             newLeague.id = documentId
             self.createAdminMembership(leagueId: documentId)
-            self.createInitialSeason(leagueId: documentId)
           }
           self.newLeagueViewControllerDelegate?.appendNewLeague(newLeague)
           
@@ -60,29 +60,59 @@ class NewLeagueViewController: UIViewController {
   }
   
   func createAdminMembership(leagueId: String) {
+    guard let userId = Auth.auth().currentUser?.uid else {
+      print("Error: NewLeagueVC#createAdminMembership: no current user")
+      return
+    }
+    
     let membershipRef = db.collection("memberships")
     var ref: DocumentReference? = nil
-    ref = membershipRef.addDocument(data: ["league_id": "asdf", "user_id": "fdsa"]) { error in
+    ref = membershipRef.addDocument(data: ["league_id": leagueId, "user_id": userId, "role": 1]) { error in
       guard error == nil else {
         print("Error: NewLeagueVC#createAdminMembership: \(error!)")
         return
       }
-      // do something with the membership?
-      // return the membershipId so it can be passed to the db call? no. probably not.
+      
+      if let membershipId = ref?.documentID {
+        print("Created initial admin: \(membershipId)")
+        self.createInitialSeason(membershipId: membershipId, leagueId: leagueId)
+      }
     }
   }
   
-  func createInitialSeason(leagueId: String) {
-    let seasonRef = db.collection("seasons")
-    var ref: DocumentReference? = nil
-    ref = seasonRef.addDocument(data: [ "league_id": leagueId, "isActive": true, "completed": false ]) { error in
-      if let error = error {
-        print("Error creating initial season: \(error)")
-      } else {
-        if let documentId = ref?.documentID {
-          print("Created Initial Season: \(documentId)")
-        }
+  func createInitialSeason(membershipId: String, leagueId: String) {
+    let membershipRef = db.collection("memberships")
+    membershipRef.document(membershipId).getDocument { document, error in
+      guard let document = document, document.exists == true else {
+        print("Error: NewLeagueVC#createInitialSeason: \(error!)")
+        return
       }
+      
+      // convert to Membership Model
+      do {
+        let membership = try FirebaseDecoder().decode(Membership.self, from: document.data()!)
+        guard membership.role == 1 else {
+          print("Error: NewLeagueVC#createInitialSeason: User is not admin on league")
+          return
+        }
+        let seasonRef = self.db.collection("seasons")
+        var ref: DocumentReference? = nil
+        let initialSeason: [String: Any] = [ "league_id": membership.league_id, "isActive": true, "completed": false ]
+        ref = seasonRef.addDocument(data: initialSeason) { error in
+          guard error == nil else {
+            print("Error: NewLeagueVC#createInitialSeason: Error creating initial season")
+            return
+          }
+          
+          if let documentId = ref?.documentID {
+            print("Created intial season: \(documentId)")
+          }
+        }
+        
+      } catch {
+        print("Error: NewLeagueVC#createInitialSeason: converting getDocument to Membership Model")
+      }
+      
     }
   }
   
